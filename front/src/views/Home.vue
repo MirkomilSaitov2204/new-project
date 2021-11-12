95b1ce648f0ef3015a8ee1cd658c78fa00ec10e0
Mirkomil M. Saitov <m.saitov@uzinfocom.uz>
2021-11-12 16:36:21 +0500

    elementio

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
-
+    <ElRow :gutter="12">
+      <ElCol :md="4" :xl="4">Mirkomil Saitov Mirkomil Saitov Mirkomil Saitov</ElCol>
+      <ElCol :md="4" :xl="4">Mirkomil Saitov Mirkomil Saitov Mirkomil Saitov</ElCol>
+      <ElCol :md="4" :xl="4">Mirkomil Saitov Mirkomil Saitov Mirkomil Saitov</ElCol>
+      <ElCol :md="4" :xl="4">Mirkomil Saitov Mirkomil Saitov Mirkomil Saitov</ElCol>
+      <ElCol :md="4" :xl="4">Mirkomil Saitov Mirkomil Saitov Mirkomil Saitov</ElCol>
+      <ElCol :md="4" :xl="4">Mirkomil Saitov Mirkomil Saitov Mirkomil Saitov</ElCol>
+    </ElRow>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: "Home",
   components: {},
